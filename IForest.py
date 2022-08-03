f9b38d9cb8b3f12f9fb6e73c5c21ee6addd559c7
VaradhKaushik <varadhkaushik@gmail.com>
2022-08-04 02:43:20 +0530

    init method created for IForest

--- IForest.py
+++ IForest.py
@@ -6,7 +6,17 @@ import pandas as pd
 
 class IForest():
 
-    def __init__():
-        pass
-
+    def __init__(self, df, max_dep=25, seed=14, *args, **kwargs):
+        self.df = df
+        self.max_dep = max_dep
+        self.seed = seed
     
+    def __str__(self):
+        return f"\nIsolation Forest object with max_depth: {self.max_dep} and seed: {self.seed}\n"
+    
+    
+
+df = [1,2,3]
+
+test = IForest(df)
+print(test)
