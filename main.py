4ed50c09070dcf222449a2f76a2d63e4921a6487
VaradhKaushik <varadhkaushik@gmail.com>
2022-08-14 17:50:55 +0530

    Made datagen function

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 from iforest import *
 
-
 np.random.seed(14)
+
 ##### Generating Test Dataset #####         - Cited in references
-mean = [0, 0]
-cov = [[1, 0], [0, 1]]  # diagonal covariance
-Nobjs = 2000
+def generate_data(n=2000):
+    mean = [0, 0]
+    cov = [[1, 0], [0, 1]]  # diagonal covariance
 
-x, y = np.random.multivariate_normal(mean, cov, Nobjs).T
-x[0], y[0] = 3.3, 3.3       #Add manual outlier
-x[100], y[100] = 4.5, 3
+    x, y = np.random.multivariate_normal(mean, cov, n).T
+    x[0], y[0] = 3.3, 3.3       #Add manual outlier
+    x[100], y[100] = 4.5, 3
 
-df=np.array([x,y]).T
-df = pd.DataFrame(df,columns=['feat1','feat2'])
+    df=np.array([x,y]).T
+    df = pd.DataFrame(df,columns=['feat1','feat2'])
 
-plt.figure("Generated Dataset")
-plt.title(f"Random Generated Dataset with {Nobjs} points")
-plt.xlabel("Feature 1 of df")
-plt.ylabel("Feature 2 of df")
-plt.scatter(x,y,c='turquoise', marker='o', alpha=0.7)
+    plt.figure("Generated Dataset")
+    plt.title(f"Random Generated Dataset with {n} points")
+    plt.xlabel("Feature 1 of df")
+    plt.ylabel("Feature 2 of df")
+    plt.scatter(x,y,c='turquoise', marker='o', alpha=0.7)
 
+    return df
+
+df = generate_data()
 
 ##### Calling the algorithms #####
 iforest_pred(df=df, subspace=64, cntm=0.01, seed=14)
@@ -38,5 +41,6 @@ try:
 except KeyError:
     print("Isolation Forest anomaly data not found")
 
+# def visualise():
 
 plt.show()
